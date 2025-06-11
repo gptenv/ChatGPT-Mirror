8a29e16005cdcb02bc01c0a0c50ae964c78fa910
Jeremy Carter <defcron@gptenv.com>
2025-06-11 21:30:31 +0200

    Minor adjustment

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ docker compose pull
 
 docker compose up -d
 
-docker images |grep chatgpt-mirror|grep dairoot|grep none |awk '{print $3}' | xargs docker rmi > /dev/null 2>&1
+docker images |grep dairoot|grep none |awk '{print $3}' | xargs docker rmi > /dev/null 2>&1
