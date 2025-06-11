ad596d5e97760cbd8b78879fe8bdc892476086ba
Jeremy Carter <defcron@gptenv.com>
2025-06-11 17:36:52 +0200

    Minor updates

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 docker compose pull
 
 docker compose up -d
 
-docker images |grep dairoot|grep none |awk '{print $3}' | xargs docker rmi > /dev/null 2>&1
+docker images |grep chatgpt-mirror|grep dairoot|grep none |awk '{print $3}' | xargs docker rmi > /dev/null 2>&1
