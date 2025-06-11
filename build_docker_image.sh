0c51b6a6c03370a2c765fc1b4a89fa778ae2111b
Jeremy Carter <defcron@gptenv.com>
2025-06-11 20:37:34 +0200

    Some default config changes and .gitignore the data/ directory.

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -3,4 +3,6 @@
 image=gptenv/chatgpt-mirror
 
 # docker buildx build --platform linux/amd64 --push -t ${image} .
-docker buildx build --platform linux/amd64,linux/arm64 -t ${image}:dev -t ${image}:0.1.0 -t ${image}:latest .
+
+#docker buildx build --platform linux/amd64,linux/arm64 -t ${image}:dev -t ${image}:0.1.0 -t ${image}:latest .
+docker buildx build --platform linux/amd64 -t ${image}:dev -t ${image}:0.1.0 -t ${image}:latest .
